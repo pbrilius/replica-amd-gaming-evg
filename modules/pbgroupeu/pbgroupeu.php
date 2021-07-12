4dfeaa6ef62217b37a515f00ae9b58baafbff25b
Povilas <pbrilius@gmail.com>
2021-07-12 19:13:52 +0300

    Attempt to construct & display UX - UI cases.

--- modules/pbgroupeu/pbgroupeu.php
+++ modules/pbgroupeu/pbgroupeu.php
@@ -58,4 +58,86 @@ class pbgroupeu extends Module
 
     return true;
   }
+
+  public function getContent()
+  {
+    $output = null;
+
+    if (Tools::isSubmit('submit'.$this->name)) {
+        $myModuleName = strval(Tools::getValue('MYMODULE_NAME'));
+
+        if (
+            !$myModuleName ||
+            empty($myModuleName) ||
+            !Validate::isGenericName($myModuleName)
+        ) {
+            $output .= $this->displayError($this->l('Invalid Configuration value'));
+        } else {
+            Configuration::updateValue('MYMODULE_NAME', $myModuleName);
+            $output .= $this->displayConfirmation($this->l('Settings updated'));
+        }
+    }
+
+    return $output.$this->displayForm();
+  }
+
+  public function displayForm()
+  {
+      // Get default language
+      $defaultLang = (int)Configuration::get('PS_LANG_DEFAULT');
+
+      // Init Fields form array
+      $fieldsForm[0]['form'] = [
+          'legend' => [
+              'title' => $this->l('Settings'),
+          ],
+          'input' => [
+              [
+                  'type' => 'text',
+                  'label' => $this->l('Configuration value'),
+                  'name' => 'MYMODULE_NAME',
+                  'size' => 20,
+                  'required' => true
+              ]
+          ],
+          'submit' => [
+              'title' => $this->l('Save'),
+              'class' => 'btn btn-default pull-right'
+          ]
+      ];
+
+      $helper = new HelperForm();
+
+      // Module, token and currentIndex
+      $helper->module = $this;
+      $helper->name_controller = $this->name;
+      $helper->token = Tools::getAdminTokenLite('AdminModules');
+      $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+
+      // Language
+      $helper->default_form_language = $defaultLang;
+      $helper->allow_employee_form_lang = $defaultLang;
+
+      // Title and toolbar
+      $helper->title = $this->displayName;
+      $helper->show_toolbar = true;        // false -> remove toolbar
+      $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
+      $helper->submit_action = 'submit'.$this->name;
+      $helper->toolbar_btn = [
+          'save' => [
+              'desc' => $this->l('Save'),
+              'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+              '&token='.Tools::getAdminTokenLite('AdminModules'),
+          ],
+          'back' => [
+              'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+              'desc' => $this->l('Back to list')
+          ]
+      ];
+
+      // Load current value
+      $helper->fields_value['MYMODULE_NAME'] = Tools::getValue('MYMODULE_NAME', Configuration::get('MYMODULE_NAME'));
+
+      return $helper->generateForm($fieldsForm);
+  }
 }
